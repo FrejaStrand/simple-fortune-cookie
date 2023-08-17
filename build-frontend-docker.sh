3ad389eb6b67004ff080015904ef1adb7c894ab8
GustavFrandsen <gufra21@Student.sdu.dk>
2023-08-17 11:28:54 +0000

    stealing went wrong forgot to change file

--- build-frontend-docker.sh
+++ build-frontend-docker.sh
@@ -2,4 +2,4 @@
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
-docker build -t "${DockerRepo}frontend-fortune-app:latest" -t "${DockerRepo}frontend-fortune-app:1.0-$Tag" app/
+docker build -t "${DockerRepo}frontend-fortune-app:latest" -t "${DockerRepo}frontend-fortune-app:1.0-$Tag" frontend/
